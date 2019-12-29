3359b16164b677849f092d3f25f1fc225630c073
James Nakoda <aslan.jnn@gmail.com>
2019-12-30 10:09:22 +1100

    First version

--- README.md
+++ README.md
@@ -1,1 +1,49 @@
 # ServiceDisabler
+
+This simple Windows service will periodically monitor and stop other services in local system. List of services can be configured, and so does the interval. This service will not disable or change the startup type of the specified services.
+
+This service requires .NET framework 4.0.
+
+## Installation
+
+Use .NET's `installutil.exe` to install or uninstall this service. By default this service will be installed with `LocalSystem` privileges.
+
+The binary in the releases page are compiled with AnyCPU, therefore both 32 bit and 64 bit version of .NET's `installutil.exe` should work.
+
+Example command to **install** this service:
+
+`C:\Windows\Microsoft.NET\Framework\v4.0.30319\InstallUtil.exe C:\path\to\ServiceDisabler.exe`
+
+Example command to **uninstall** this service:
+
+`C:\Windows\Microsoft.NET\Framework\v4.0.30319\InstallUtil.exe /u C:\path\to\ServiceDisabler.exe`
+
+Run those commands with administrator privilege.
+
+## Configuration
+
+All configuration data are stored in registry with the following path: ```HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\ServiceDisabler\Config```.
+
+Please note that you may need to create the `Config` key first.
+
+The registry entries are as follows:
+
+- `ServiceNames` (Required)  
+Type: `REG_SZ`  
+List of services to be monitored and autostopped. Separate each service name with semicolon (`;`). Do not begin or end with semicolon, and do not put any space inside the string.
+- `CheckFrequency` (Optional)  
+Type: `REG_DWORD`  
+Specify the frequency of monitoring, in seconds. If not specified, default to 5 seconds.
+
+## Errors
+
+If the service is misconfigured, service will fail to start and the following errors will be thrown:
+
+Code|Reason
+---|---
+`0x80070005`|Service is denied to access its configuration registry entry.
+`0x80070013`|Wrong data type is specified for configuration entries.
+`0x80004005`|Unknown error when opening configuration data.
+`0x80070003`|Cannot retreive the status for the specified service.
+
+Once this service is started successfully, this service will not throw any errors even though it may fail modifying the service status.
---
+++ ServiceDisabler/ServiceDisabler.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using Microsoft.Win32;
+using System.Timers;
+
+namespace ServiceDisabler
+{
+    public partial class ServiceDisabler : ServiceBase
+    {
+        private string[] ServiceNames { get; set; }
+        private int CheckFrequency { get; set; }
+
+        private Timer _MonitorTimer;
+
+        public ServiceDisabler()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            //Debugger.Launch();
+            // open registry settings
+            try
+            {
+                var regKey = Registry.LocalMachine.OpenSubKey($"SYSTEM\\CurrentControlSet\\Services\\{ServiceName}\\Config");
+                if (regKey == null)
+                {
+                    RaiseError(0x80070002);
+                    return;
+                }
+                var svcNames = (string)regKey.GetValue("ServiceNames");
+                ServiceNames = svcNames.Split(';');
+                CheckFrequency = (int)regKey.GetValue("CheckFrequency", 5);
+            }
+            catch(System.Security.SecurityException ex)
+            {
+                RaiseError(0x80070005);
+                return;
+            }
+            catch(InvalidCastException ex)
+            {
+                RaiseError(0x80070013);
+                return;
+            }
+            catch(Exception ex)
+            {
+                RaiseError(0x80004005);
+                return;
+            }
+
+            // check if services are available
+            try
+            {
+                for (int i = 0; i < ServiceNames.Length; ++i)
+                {
+                    var svcObj = new ServiceController(ServiceNames[i]);
+                    // trigger service not found exception :)
+                    var a = svcObj.Status;
+                }
+            }
+            catch(Exception ex)
+            {
+                RaiseError(0x80070003);
+                return;
+            }
+
+            // start the timer
+            _MonitorTimer = new Timer();
+            _MonitorTimer.Interval = CheckFrequency * 1000;
+            _MonitorTimer.Elapsed += _MonitorTimer_Elapsed;
+            _MonitorTimer.Start();
+        }
+
+        private void _MonitorTimer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            foreach(var svcName in ServiceNames)
+            {
+                var svcObj = new ServiceController(svcName);
+                try
+                {
+                    switch (svcObj.Status)
+                    {
+                        case ServiceControllerStatus.Stopped:
+                        case ServiceControllerStatus.StopPending:
+                            continue;
+                        default:
+                            svcObj.Stop();
+                            break;
+                    }
+                }
+                catch (Exception ex) { continue; }
+            }
+        }
+
+        protected override void OnStop()
+        {
+            _MonitorTimer.Stop();
+        }
+
+        private void RaiseError(uint code)
+        {
+            ExitCode = unchecked((int)code);
+            Stop();
+        }
+    }
+}
